823877b15dfa6c6fc0f7b17d9c541cb7a9ea9da5
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-03-18 18:14:22 +0100

    Format

--- packages/tensorflow/tensorflow_platform_interface/lib/tensorflow_platform_interface.dart
+++ packages/tensorflow/tensorflow_platform_interface/lib/tensorflow_platform_interface.dart
@@ -1,17 +1,13 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'src/method_channel_tensorflow.dart';
-
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'src/method_channel_tensorflow.dart';
 
 /// The interface that implementations of tensorflow must implement.
 ///
-/// Platform implementations should extend this class rather than implement it as `Tensorflow`
-/// does not consider newly added methods to be breaking changes. Extending this class
-/// (using `extends`) ensures that the subclass will get the default implementation, while
-/// platform implementations that `implements` this interface will be broken by newly added
+/// Platform implementations should extend this class rather than implement
+/// it as `Tensorflow` does not consider newly added methods to be breaking
+/// changes. Extending this class (using `extends`) ensures that the subclass
+/// will get the default implementation, while platform implementations that
+/// `implements` this interface will be broken by newly added
 /// [TensorflowPlatform] methods.
 abstract class TensorflowPlatform extends PlatformInterface {
   /// Constructs a ConnectivityPlatform.
