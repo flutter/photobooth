e2afc7aced515e6cfd062f466f13bb889bb44634
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-03-25 20:11:42 +0100

    Remove unused

--- packages/tensorflow/tensorflow_web/example/lib/main.dart
+++ packages/tensorflow/tensorflow_web/example/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: HomePage(),
     );
   }
