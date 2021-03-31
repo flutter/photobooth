15f9af21fb78db57d97dea447337e29b9a8ab0ba
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-03-31 20:00:40 +0200

    Fix size for buttons

--- lib/preview/widgets/download_button.dart
+++ lib/preview/widgets/download_button.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:io_photobooth/l10n/l10n.dart';
 
+//TODO: move to package
 class DownloadButton extends StatelessWidget {
   const DownloadButton({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    return ElevatedButton(
-      child: Text(l10n.previewPageDownloadButtonText),
-      onPressed: () {},
+    return ConstrainedBox(
+      constraints: const BoxConstraints.tightFor(
+        width: 208,
+        height: 54,
+      ),
+      child: ElevatedButton(
+        child: Text(l10n.previewPageDownloadButtonText),
+        onPressed: () {},
+      ),
     );
   }
 }
