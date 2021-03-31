911047054193b0fbdbedc3e1e93d56250c53c67e
Felix Angelov <felangelov@gmail.com>
2021-03-31 13:54:42 -0500

    feat(photobooth_ui): move theme and colors

---
+++ packages/photobooth_ui/lib/src/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+/// Defines the color palette for the Photobooth UI.
+abstract class PhotoboothColors {
+  /// Black
+  static const Color black = Color(0xFF000000);
+
+  /// White
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// Transparent
+  static const Color transparent = Color(0x00000000);
+
+  /// Light Blue
+  static const Color lightBlue = Color(0xFF13B9FF);
+}
---
+++ packages/photobooth_ui/lib/src/theme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
+
+/// {@template photo_booth_theme}
+/// Namespace for the Photobooth [ThemeData].
+/// {@endtemplate}
+class PhotoboothTheme {
+  /// Default `ThemeData` for Photobooth UI.
+  static ThemeData get themeData {
+    return ThemeData(
+      accentColor: PhotoboothColors.lightBlue,
+      appBarTheme: _appBarTheme,
+      elevatedButtonTheme: _elevatedButtonTheme,
+    );
+  }
+
+  static AppBarTheme get _appBarTheme {
+    return const AppBarTheme(color: PhotoboothColors.lightBlue);
+  }
+
+  static ElevatedButtonThemeData get _elevatedButtonTheme {
+    return ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+        padding: const EdgeInsets.all(24),
+      ),
+    );
+  }
+}
