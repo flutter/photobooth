7df09726282e5f139aa8b279df0fe251f9a1bf12
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-04-01 11:08:33 +0200

    Adding typography styles

--- packages/photobooth_ui/lib/src/theme.dart
+++ packages/photobooth_ui/lib/src/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
+import 'package:photobooth_ui/src/typography/typography.dart';
 
 /// Namespace for the Photobooth [ThemeData].
 class PhotoboothTheme {
@@ -9,8 +10,25 @@ class PhotoboothTheme {
       accentColor: PhotoboothColors.lightBlue,
       appBarTheme: _appBarTheme,
       elevatedButtonTheme: _elevatedButtonTheme,
+      textTheme: _textTheme,
     );
   }
+
+  static TextTheme get _textTheme => TextTheme(
+        headline1: VeryGoodTextStyle.headline1,
+        headline2: VeryGoodTextStyle.headline2,
+        headline3: VeryGoodTextStyle.headline3,
+        headline4: VeryGoodTextStyle.headline4,
+        headline5: VeryGoodTextStyle.headline5,
+        headline6: VeryGoodTextStyle.headline6,
+        subtitle1: VeryGoodTextStyle.subtitle1,
+        subtitle2: VeryGoodTextStyle.subtitle2,
+        bodyText1: VeryGoodTextStyle.bodyText1,
+        bodyText2: VeryGoodTextStyle.bodyText2,
+        caption: VeryGoodTextStyle.caption,
+        overline: VeryGoodTextStyle.overline,
+        button: VeryGoodTextStyle.button,
+      );
 
   static AppBarTheme get _appBarTheme {
     return const AppBarTheme(color: PhotoboothColors.lightBlue);
@@ -23,6 +41,7 @@ class PhotoboothTheme {
           borderRadius: BorderRadius.all(Radius.circular(30)),
         ),
         padding: const EdgeInsets.all(24),
+        textStyle: _textTheme.button,
       ),
     );
   }
---
+++ packages/photobooth_ui/lib/src/typography/text_styles.dart
@@ -0,0 +1,95 @@
+import 'dart:ui';
+
+import 'package:flutter/widgets.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
+
+import 'font_weights.dart';
+
+/// Very Good Text Style Definitions
+class VeryGoodTextStyle {
+  /// Headline 1 Text Style
+
+  static TextStyle get headline1 => _baseTextStyle.copyWith(
+        fontSize: 56,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  /// Headline 2 Text Style
+  static TextStyle get headline2 => _baseTextStyle.copyWith(
+        fontSize: 24,
+        fontWeight: VeryGoodFontWeight.regular,
+      );
+
+  /// Headline 3 Text Style
+  static TextStyle get headline3 => _baseTextStyle.copyWith(
+        fontSize: 24,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  /// Headline 4 Text Style
+  static TextStyle get headline4 => _baseTextStyle.copyWith(
+        fontSize: 22,
+        fontWeight: VeryGoodFontWeight.bold,
+      );
+
+  /// Headline 5 Text Style
+  static TextStyle get headline5 => _baseTextStyle.copyWith(
+        fontSize: 22,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  /// Headline 6 Text Style
+  static TextStyle get headline6 => _baseTextStyle.copyWith(
+        fontSize: 22,
+        fontWeight: VeryGoodFontWeight.bold,
+      );
+
+  /// Subtitle 1 Text Style
+  static TextStyle get subtitle1 => _baseTextStyle.copyWith(
+        fontSize: 16,
+        fontWeight: VeryGoodFontWeight.bold,
+      );
+
+  /// Subtitle 2 Text Style
+  static TextStyle get subtitle2 => _baseTextStyle.copyWith(
+        fontSize: 14,
+        fontWeight: VeryGoodFontWeight.bold,
+      );
+
+  /// Body Text 1 Text Style
+  static TextStyle get bodyText1 => _baseTextStyle.copyWith(
+        fontSize: 18,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  /// Body Text 2 Text Style (the default)
+  static TextStyle get bodyText2 => _baseTextStyle.copyWith(
+        fontSize: 16,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  /// Caption Text Style
+  static TextStyle get caption => _baseTextStyle.copyWith(
+        fontSize: 14,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  /// Overline Text Style
+  static TextStyle get overline => _baseTextStyle.copyWith(
+        fontSize: 16,
+        fontWeight: VeryGoodFontWeight.bold,
+      );
+
+  /// Button Text Style
+  static TextStyle get button => _baseTextStyle.copyWith(
+        fontSize: 22,
+        fontWeight: VeryGoodFontWeight.medium,
+      );
+
+  static const _baseTextStyle = TextStyle(
+    fontFamily: 'RedHatDisplay',
+    package: 'photobooth_ui',
+    color: PhotoboothColors.black,
+    fontWeight: VeryGoodFontWeight.regular,
+  );
+}
