52d4f130ba1e07c89d91bd343fee9c88cd5b77d5
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-04-05 22:20:15 +0200

    Fix overflow

--- lib/photobooth/widgets/photobooth_placeholder.dart
+++ lib/photobooth/widgets/photobooth_placeholder.dart
@@ -8,21 +8,23 @@ class PhotoboothPlaceholder extends StatelessWidget {
     final l10n = context.l10n;
     final theme = Theme.of(context);
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            l10n.photoBothPlaceholderHeadline,
-            style: theme.textTheme.headline1,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 24),
-          Text(
-            l10n.photoBothPlaceholderSubheadline,
-            style: theme.textTheme.headline2,
-            textAlign: TextAlign.center,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              l10n.photoBothPlaceholderHeadline,
+              style: theme.textTheme.headline1,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 24),
+            Text(
+              l10n.photoBothPlaceholderSubheadline,
+              style: theme.textTheme.headline2,
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
