4c767606dd3ecb8a54c4642f301aa3943d548b6b
David DeRemer <david@verygood.ventures>
2021-04-05 16:00:42 -0400

    feat(functions): add functions (#89)

---
+++ functions/src/index.ts
@@ -0,0 +1,10 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const share = functions.https.onRequest((_request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+
+  response.send("Hello from Firebase!");
+});
