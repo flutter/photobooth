ec9e2a25b378c9340b489a2d5d4a2622a4402054
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-04-06 19:42:39 +0200

    Remove stateful

--- lib/decoration/view/decoration_page.dart
+++ lib/decoration/view/decoration_page.dart
@@ -4,32 +4,22 @@ import 'package:io_photobooth/decoration/decoration.dart';
 import 'package:io_photobooth/preview/preview.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
 
-class DecorationPage extends StatefulWidget {
+class DecorationPage extends StatelessWidget {
   const DecorationPage({Key? key, required this.image}) : super(key: key);
-
   final ImageData image;
 
-  static Route route({required ImageData image}) {
-    return MaterialPageRoute(builder: (_) => DecorationPage(image: image));
-  }
-
-  @override
-  _DecorationPageState createState() => _DecorationPageState();
-}
-
-class _DecorationPageState extends State<DecorationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: [
-          PreviewImage(data: widget.image.data),
+          PreviewImage(data: image.data),
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
               padding: const EdgeInsets.only(bottom: 15),
-              child: GoToPreviewButton(image: widget.image),
+              child: GoToPreviewButton(image: image),
             ),
           ),
           const Align(
