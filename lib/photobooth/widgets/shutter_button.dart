ceae3b251a3eb3abba3deb712b1c85bf42af65f3
Oscar Martín Martín <martinm.oscar@gmail.com>
2021-04-08 19:22:16 +0200

    Circular animation

--- lib/photobooth/widgets/shutter_button.dart
+++ lib/photobooth/widgets/shutter_button.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
 
@@ -13,10 +14,11 @@ class _ShutterButtonState extends State<ShutterButton>
   var displayAnimation = false;
   late AnimationController controller;
   String get timerString {
-    final duration = controller.duration! * controller.value;
-
-    print(duration.inSeconds.toString().padLeft(width));
-    return '${(duration.inSeconds % 60).toString().padLeft(1, '0')}';
+    var duration = (controller.duration! * controller.value).inSeconds;
+    if (duration != 3) {
+      duration = duration + 1;
+    }
+    return '${(duration % 60).toString().padLeft(1, '0')}';
   }
 
   @override
@@ -35,24 +37,68 @@ class _ShutterButtonState extends State<ShutterButton>
       builder: (context, child) {
         if (controller.isAnimating) {
           return Container(
-            color: Colors.amber,
-            child: Text(
-              timerString,
-              style: const TextStyle(
-                fontSize: 74,
-                fontWeight: FontWeight.w500,
-                color: PhotoboothColors.white,
-              ),
+            height: 100,
+            width: 100,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    timerString,
+                    style: const TextStyle(
+                      fontSize: 74,
+                      fontWeight: FontWeight.w500,
+                      color: PhotoboothColors.white,
+                    ),
+                  ),
+                ),
+                Positioned.fill(
+                  child: CustomPaint(
+                    painter: TimerPainter(
+                      animation: controller,
+                    ),
+                  ),
+                )
+              ],
             ),
           );
         } else {
           return FloatingActionButton(
             onPressed: () {
-              controller.forward();
+              controller.reverse(
+                from: 1,
+              );
             },
           );
         }
       },
     );
+  }
+}
+
+class TimerPainter extends CustomPainter {
+  TimerPainter({
+    required this.animation,
+  }) : super(repaint: animation);
+
+  final Animation<double> animation;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = PhotoboothColors.white
+      ..strokeWidth = 5.0
+      ..strokeCap = StrokeCap.round
+      ..style = PaintingStyle.stroke;
+
+    canvas.drawCircle(size.center(Offset.zero), size.width / 2.0, paint);
+    paint.color = Colors.red;
+    final progress = (1.0 - animation.value) * 2 * math.pi;
+    canvas.drawArc(Offset.zero & size, math.pi * 1.5, -progress, false, paint);
+  }
+
+  @override
+  bool shouldRepaint(dynamic oldDelegate) {
+    return true;
   }
 }
