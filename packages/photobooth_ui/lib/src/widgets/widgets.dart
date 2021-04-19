662174e9bfcce7dc90ce211f0acf379fb827182e
Maciej Korzeniewski <maciej@korzeniewski.dev>
2021-04-19 21:16:56 +0200

    feat(photobooth_ui): add clickable widget

--- packages/photobooth_ui/lib/src/widgets/widgets.dart
+++ packages/photobooth_ui/lib/src/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'adaptive_builder.dart';
+export 'clickable.dart';
 export 'draggable_resizable_asset/draggable_resizable_asset.dart';
 export 'platform_builder.dart';
 export 'preview_image.dart';
