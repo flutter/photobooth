d5d150ebdbb1795019ee9273a98f04be221a6926
Maciej Korzeniewski <maciej@korzeniewski.dev>
2021-04-21 15:26:11 +0200

    feat: integrate with firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 import 'dart:developer';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:bloc/bloc.dart';
 import 'package:io_photobooth/app/app.dart';
 import 'package:io_photobooth/app/app_bloc_observer.dart';
 import 'package:io_photobooth/assets/assets.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = AppBlocObserver();
   FlutterError.onError = (details) {
@@ -16,7 +18,10 @@ void main() {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  runZonedGuarded(
+  await Firebase.initializeApp();
+  await FirebaseAuth.instance.signInAnonymously();
+
+  await runZonedGuarded(
     () => Assets.load().then((_) => runApp(const App())),
     (error, stackTrace) {
       print(error.toString());
