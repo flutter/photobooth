7105cfaafd90be611e6cb20c12dda1d4910320c8
Maciej Korzeniewski <maciej@korzeniewski.dev>
2021-04-21 21:14:06 +0200

    define dialog radius in theme

--- lib/share/view/share_dialog.dart
+++ lib/share/view/share_dialog.dart
@@ -16,82 +16,76 @@ class ShareDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final l10n = context.l10n;
-    final borderRadius = BorderRadius.circular(12);
     return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: borderRadius,
-      ),
-      child: ClipRRect(
-        borderRadius: borderRadius,
-        child: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                PhotoboothColors.whiteBackground,
-                PhotoboothColors.white,
-              ],
-            ),
-          ),
-          child: Stack(
-            children: [
-              SingleChildScrollView(
-                child: Container(
-                  width: 900,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30,
-                      vertical: 60,
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 300,
-                          child: PhotoboothPhoto(image: image.data),
-                        ),
-                        const SizedBox(height: 60),
-                        Text(
-                          l10n.shareDialogHeading,
-                          style: theme.textTheme.headline1,
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 24),
-                        Text(
-                          l10n.shareDialogSubheading,
-                          style: theme.textTheme.headline2,
-                          textAlign: TextAlign.center,
-                        ),
-                        const SizedBox(height: 42),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const TwitterButton(),
-                            const SizedBox(width: 36),
-                            const FacebookButton(),
-                          ],
-                        ),
-                        const SizedBox(height: 16),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                left: 24,
-                top: 24,
-                child: IconButton(
-                  icon: Icon(
-                    Icons.clear,
-                    color: PhotoboothColors.black.withOpacity(0.54),
-                  ),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ),
+      clipBehavior: Clip.hardEdge,
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              PhotoboothColors.whiteBackground,
+              PhotoboothColors.white,
             ],
           ),
+        ),
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Container(
+                width: 900,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 30,
+                    vertical: 60,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 300,
+                        child: PhotoboothPhoto(image: image.data),
+                      ),
+                      const SizedBox(height: 60),
+                      Text(
+                        l10n.shareDialogHeading,
+                        style: theme.textTheme.headline1,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 24),
+                      Text(
+                        l10n.shareDialogSubheading,
+                        style: theme.textTheme.headline2,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 42),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const TwitterButton(),
+                          const SizedBox(width: 36),
+                          const FacebookButton(),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 24,
+              top: 24,
+              child: IconButton(
+                icon: Icon(
+                  Icons.clear,
+                  color: PhotoboothColors.black.withOpacity(0.54),
+                ),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ),
+          ],
         ),
       ),
     );
