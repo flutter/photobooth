d1f4aea7756f3d39782dfc604e74fff12a1bf3cb
Felix Angelov <felangelov@gmail.com>
2021-04-22 13:40:39 -0500

    fix: greyscale filter fix for character buttons (#250)

--- lib/photobooth/widgets/photobooth_preview.dart
+++ lib/photobooth/widgets/photobooth_preview.dart
@@ -195,9 +195,13 @@ class CharacterIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const disabledColorFilter = ColorFilter.mode(
-      PhotoboothColors.gray,
-      BlendMode.saturation,
+    const disabledColorFilter = ColorFilter.matrix(
+      <double>[
+        0.2126, 0.7152, 0.0722, 0, 0, //
+        0.2126, 0.7152, 0.0722, 0, 0, //
+        0.2126, 0.7152, 0.0722, 0, 0, //
+        0, 0, 0, 1, 0, //
+      ],
     );
     return Padding(
       padding: const EdgeInsets.all(12),
