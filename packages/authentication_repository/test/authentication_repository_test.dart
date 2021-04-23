c96b068e679daa31341f14c159b65348835b1091
Maciej Korzeniewski <maciej@korzeniewski.dev>
2021-04-23 12:41:43 +0200

    test: simplify tests

--- packages/authentication_repository/test/authentication_repository_test.dart
+++ packages/authentication_repository/test/authentication_repository_test.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: must_be_immutable
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -20,38 +18,6 @@ class MockUserCredential extends Mock implements firebase_auth.UserCredential {}
 class FakeAuthCredential extends Fake implements firebase_auth.AuthCredential {}
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-  MethodChannelFirebase.channel.setMockMethodCallHandler((call) async {
-    if (call.method == 'Firebase#initializeCore') {
-      return [
-        {
-          'name': defaultFirebaseAppName,
-          'options': {
-            'apiKey': '123',
-            'appId': '123',
-            'messagingSenderId': '123',
-            'projectId': '123',
-          },
-          'pluginConstants': const <String, String>{},
-        }
-      ];
-    }
-
-    if (call.method == 'Firebase#initializeApp') {
-      final arguments = call.arguments as Map<String, dynamic>;
-      return <String, dynamic>{
-        'name': arguments['appName'],
-        'options': arguments['options'],
-        'pluginConstants': const <String, String>{},
-      };
-    }
-
-    return null;
-  });
-
-  TestWidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
-
   const user = User(id: _mockFirebaseUserUid);
 
   group('AuthenticationRepository', () {
