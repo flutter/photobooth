f090b16f9ec1e4c640aa93375588169db5bc88a7
Brian Tobin <brianjtobin@gmail.com>
2021-04-26 14:35:59 -0400

    feat(functions): sharing function styling (#307)

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,3 +1,8 @@
 export const ENV = process.env.NODE_ENV;
 export const UPLOAD_PATH = 'uploads';
 export const SHARE_PATH = 'share';
+export const ALLOWED_HOSTS = [
+  'localhost:5001',
+  'io-photobooth-dev.web.app',
+  'io-photo-booth.web.app',
+];
