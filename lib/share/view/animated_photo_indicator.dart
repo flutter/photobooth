7552adb7873f364f6bae79b75d0c69efeb72af10
Maciej Korzeniewski <maciej@korzeniewski.dev>
2021-04-27 12:35:02 +0200

    refactor: simplify animated photo indicator

--- lib/share/view/animated_photo_indicator.dart
+++ lib/share/view/animated_photo_indicator.dart
@@ -1,45 +1,18 @@
-import 'package:photobooth_ui/photobooth_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:platform_helper/platform_helper.dart';
+import 'package:photobooth_ui/photobooth_ui.dart';
 
 class AnimatedPhotoIndicator extends StatelessWidget {
-  AnimatedPhotoIndicator({
-    Key? key,
-    PlatformHelper? platformHelper,
-  })  : platformHelper = platformHelper ?? PlatformHelper(),
-        super(key: key);
-
-  /// Optional [PlatformHelper] instance.
-  final PlatformHelper platformHelper;
+  AnimatedPhotoIndicator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (platformHelper.isMobile) {
-      return const SizedBox(
-        width: 104,
-        height: 124,
-        child: AnimatedSprite(
-          sprites: Sprites(
-            asset: 'photo_indicator_spritesheet_mobile.png',
-            size: Size(250, 239),
-            frames: 51,
-            stepTime: 0.05,
-          ),
-        ),
-      );
-    } else {
-      return const SizedBox(
-        width: 136,
-        height: 160,
-        child: AnimatedSprite(
-          sprites: Sprites(
-            asset: 'photo_indicator_spritesheet_desktop.png',
-            size: Size(173, 163),
-            frames: 51,
-            stepTime: 0.05,
-          ),
-        ),
-      );
-    }
+    return const AnimatedSprite(
+      sprites: Sprites(
+        asset: 'photo_indicator_spritesheet.png',
+        size: Size(173, 163),
+        frames: 51,
+        stepTime: 0.05,
+      ),
+    );
   }
 }
