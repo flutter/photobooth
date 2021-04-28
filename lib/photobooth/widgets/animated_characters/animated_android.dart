3b9ad86ce71be4a9042558e9203905a0fab72b3c
Felix Angelov <felangelov@gmail.com>
2021-04-28 17:21:16 -0500

    feat: update character assets

--- lib/photobooth/widgets/animated_characters/animated_android.dart
+++ lib/photobooth/widgets/animated_characters/animated_android.dart
@@ -7,7 +7,7 @@ class AnimatedAndroid extends AnimatedSprite {
           key: key,
           sprites: const Sprites(
             asset: 'android_spritesheet.png',
-            size: Size(875, 861),
+            size: Size(592, 860),
             frames: 25,
           ),
         );
