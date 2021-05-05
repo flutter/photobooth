1279375df31b1533d96b5c22895e122873a958d6
Maciej Korzeniewski <maciej@korzeniewski.dev>
2021-05-05 15:29:19 +0200

    refactor: rename app_view to app_page_view

--- packages/photobooth_ui/lib/src/widgets/widgets.dart
+++ packages/photobooth_ui/lib/src/widgets/widgets.dart
@@ -1,9 +1,9 @@
 export 'animated_sprite.dart';
 export 'app_circular_progress_indicator.dart';
 export 'app_dialog.dart';
+export 'app_page_view.dart';
 export 'app_tooltip.dart';
 export 'app_tooltip_button.dart';
-export 'app_view.dart';
 export 'clickable.dart';
 export 'draggable_resizable.dart';
 export 'platform_builder.dart';
