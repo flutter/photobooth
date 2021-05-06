3691a7dc628d282e6a866f55b1c1f56567429f12
David Iglesias <ditman@gmail.com>
2021-05-06 16:40:07 -0700

    refactor: use cross_file directly instead of through file_picker. (#479)

--- lib/share/bloc/share_bloc.dart
+++ lib/share/bloc/share_bloc.dart
@@ -4,7 +4,7 @@ import 'dart:typed_data';
 import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
 import 'package:equatable/equatable.dart';
-import 'package:file_selector/file_selector.dart';
+import 'package:cross_file/cross_file.dart';
 import 'package:image_compositor/image_compositor.dart';
 import 'package:io_photobooth/photobooth/photobooth.dart';
 import 'package:photos_repository/photos_repository.dart';
