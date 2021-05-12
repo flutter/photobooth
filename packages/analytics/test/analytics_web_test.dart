fb72ec6041e20b0f00d1323034f94f51c7a358ef
Felix Angelov <felangelov@gmail.com>
2021-05-12 11:25:44 -0500

    feat: add google analytics event tracking

---
+++ packages/analytics/test/analytics_web_test.dart
@@ -0,0 +1,40 @@
+@TestOn('chrome')
+
+import 'dart:js';
+
+import 'package:analytics/src/analytics_web.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockJsObject extends Mock implements JsObject {}
+
+void main() {
+  group('trackEvent', () {
+    late JsObject context;
+
+    setUp(() {
+      context = MockJsObject();
+      testContext = context;
+    });
+
+    test('calls ga on window with correct args', () {
+      const category = 'category';
+      const action = 'action';
+      const label = 'label';
+      expect(
+        () => trackEvent(
+          category: category,
+          action: action,
+          label: label,
+        ),
+        returnsNormally,
+      );
+      verify(
+        () => context.callMethod(
+          'ga',
+          <dynamic>['send', 'event', category, action, label],
+        ),
+      ).called(1);
+    });
+  });
+}
