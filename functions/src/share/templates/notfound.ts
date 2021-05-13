60eddb76fe4431723081a47e3de8efac134486f4
Brian Tobin <brianjtobin@gmail.com>
2021-05-13 17:26:40 -0400

    fix(function): 404 title and subtitle text (#609)

--- functions/src/share/templates/notfound.ts
+++ functions/src/share/templates/notfound.ts
@@ -36,9 +36,8 @@ export default `
         <img src="{{{assetUrls.notFoundPhoto}}}">
       </div>
       <div class="text">
-        <h1>Photo taken with Flutter</h1>
-        <h2>Join the fun! Grab a photo with your favorite Google mascot 
-          at the I/O Photo Booth.</h2>
+        <h1>Taken with I/O Photo Booth</h1>
+        <h2>Oops! This photo is gone, but that doesn't mean the fun has to end.</h2>
         <a class="share-btn" href="/">Take your own</a>
       </div>
     </main>
