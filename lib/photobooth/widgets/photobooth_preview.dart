2cd33ed21154008271d41f1de14d5c78cfe17a6b
Felix Angelov <felangelov@gmail.com>
2021-05-18 16:39:41 -0500

    feat: add various semantics labels (#637)

--- lib/photobooth/widgets/photobooth_preview.dart
+++ lib/photobooth/widgets/photobooth_preview.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:io_photobooth/assets.g.dart';
 import 'package:io_photobooth/footer/footer.dart';
+import 'package:io_photobooth/l10n/l10n.dart';
 import 'package:io_photobooth/photobooth/photobooth.dart';
 import 'package:photobooth_ui/photobooth_ui.dart';
 
@@ -21,12 +22,13 @@ class PhotoboothPreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final l10n = context.l10n;
     final state = context.watch<PhotoboothBloc>().state;
     final children = <Widget>[
       CharacterIconButton(
         key: const Key('photoboothView_dash_characterIconButton'),
         icon: const AssetImage('assets/icons/dash_icon.png'),
-        label: 'Dash',
+        label: l10n.dashButtonLabelText,
         isSelected: state.isDashSelected,
         onPressed: () {
           trackEvent(
@@ -42,7 +44,7 @@ class PhotoboothPreview extends StatelessWidget {
       CharacterIconButton(
         key: const Key('photoboothView_sparky_characterIconButton'),
         icon: const AssetImage('assets/icons/sparky_icon.png'),
-        label: 'Sparky',
+        label: l10n.sparkyButtonLabelText,
         isSelected: state.isSparkySelected,
         onPressed: () {
           trackEvent(
@@ -58,7 +60,7 @@ class PhotoboothPreview extends StatelessWidget {
       CharacterIconButton(
         key: const Key('photoboothView_android_characterIconButton'),
         icon: const AssetImage('assets/icons/android_icon.png'),
-        label: 'Android Jetpack',
+        label: l10n.androidButtonLabelText,
         isSelected: state.isAndroidSelected,
         onPressed: () {
           trackEvent(
@@ -74,7 +76,7 @@ class PhotoboothPreview extends StatelessWidget {
       CharacterIconButton(
         key: const Key('photoboothView_dino_characterIconButton'),
         icon: const AssetImage('assets/icons/dino_icon.png'),
-        label: 'Dino',
+        label: l10n.dinoButtonLabelText,
         isSelected: state.isDinoSelected,
         onPressed: () {
           trackEvent(
